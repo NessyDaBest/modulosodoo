b5a639c84a17071c6b5f9ff663ef48018e9709ba
NessyDaBest <nessblack03@gmail.com>
2024-02-14 22:07:53 +0100

    primer ejercicio de añadir los booleanos y las vie

--- videoclub_nestor/models/videoclub.py
+++ videoclub_nestor/models/videoclub.py
@@ -11,3 +11,28 @@ class videoclub_pelis(models.Model):
     clasificacion = fields.Selection([('TP','Todos los Públicos'),('men12','Menores de12 años'),('may18','Mayores 18 años')], string='Clasificación', default='TP')
     presupuesto = fields.Integer()
     fechaestreno = fields.Date()
+
+     # Se subvenciona el 30% de la película
+    subvencionado = fields.Integer(compute='_valor_subvencion')
+    invertido = fields.Integer(compute='_valor_inversion', store=False)  # queremos verlo de modo inmediato
+    
+    # Campo calculado para determinar si es millonario
+    millonario = fields.Boolean(compute='_es_millonario', store=False, readonly=True)
+
+    # Nuevo campo de imagen
+    imagen = fields.Image("imagen")
+
+    @api.depends('presupuesto')
+    def _valor_subvencion(self):
+        for record in self:
+            record.subvencionado = record.presupuesto * 0.3
+    
+    @api.depends('presupuesto')
+    def _valor_inversion(self):
+        for record in self:
+            record.invertido = record.presupuesto * 0.7
+            
+    @api.depends('presupuesto')
+    def _es_millonario(self):
+        for record in self:
+            record.millonario = record.presupuesto > 1000000
